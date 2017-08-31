b6c1c638442474578971acb5ff27aad7ca9aa16b
Cole McKnight <cbmckni@clemson.edu>
2017-08-31 13:33:29 -0400

    produces GEMs - getting close

--- power8-gem.sh
+++ power8-gem.sh
@@ -5,26 +5,32 @@ REF=$1
 BASEDIR=$(pwd)
 
 printf "Step 0:Index Genome"
-chmod +x $BASEDIR/01-Prepare-inputs.sh
-source Index-Genome.sh "$REF" > $BASEDIR/Logs/00.out
+chmod +x $BASEDIR/Index-Genome.sh
+time ./Index-Genome.sh "$REF" > $BASEDIR/Logs/00.out
 
+cd $BASEDIR
 printf "Step 1:Prepare Inputs"
 chmod +x $BASEDIR/01-Prepare-inputs.sh
-source 01-Prepare-inputs.sh > $BASEDIR/Logs/01.out
+time ./01-Prepare-inputs.sh > $BASEDIR/Logs/01.out
 
+cd $BASEDIR
 printf "Step 2:Trim Reads"
 chmod +x $BASEDIR/02-Trim-reads.sh
-source 02-Trim-reads.sh > $BASEDIR/Logs/02.out
+time ./02-Trim-reads.sh > $BASEDIR/Logs/02.out
 
+cd $BASEDIR
 printf "Step 3:Map Reads"
 chmod +x $BASEDIR/03-Map-reads.sh
-source 03-Map-reads.sh "$REF" > $BASEDIR/Logs/03.out
+time ./03-Map-reads.sh "REF" > $BASEDIR/Logs/03.out
 
+cd $BASEDIR
 printf "Step 4:Count transcripts"
 chmod +x $BASEDIR/04-Count-transcripts.sh
-source 04-Count-transcripts.sh > $BASEDIR/Logs/04.out
+time ./04-Count-transcripts.sh > $BASEDIR/Logs/04.out
 
+cd $BASEDIR
 printf "Step 5:GEM Parse"
 chmod +x $BASEDIR/05-GEM-parse.sh
-source 05-GEM-parse.sh > $BASEDIR/Logs/05.out
+time ./05-GEM-parse.sh > $BASEDIR/Logs/05.out 
+
 
